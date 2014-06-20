9b8a5d89d80990a29e80eaccecc3e82555da088d
Adam Florczak <adam.florczak@me.com>
2014-06-20 12:28:43 +0200

    Pronounceable option generates separated string

--- dist/js/zen.js
+++ dist/js/zen.js
@@ -13,7 +13,39 @@ $(function() {
     var password = generatePassword(length, is_pronounceable);
     var strength = zxcvbn(password);
 
+    if (is_pronounceable) {
+      password = separateString(password, '-');
+    }
+
     output.val(password);
-    strenth_output.text(strength.crack_time_display);
+    strength_output.text(strength.crack_time_display);
   });
+
+  setTimeout(function() {
+    $('#generate-btn').trigger('click');
+  }, 20);
 });
+
+function separateString(string, separator) {
+  string.split('');
+  var output = [];
+
+  // randomize position of first separator
+  var split_index = randomNumber(5, 2);
+
+  for (var i = 0; i < string.length; i++) {
+    output.push(string[i]);
+
+    // add separator and increment index for next position
+    if (i == split_index && i != string.length-1) {
+      output.push(separator);
+      split_index += randomNumber(5, 2);
+    }
+  }
+
+  return output.join('');
+}
+
+function randomNumber(max, min) {
+  return (Math.floor(Math.random() * 100) % (max - min)) + min;
+}
