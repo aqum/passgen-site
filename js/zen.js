ca6e458a481d5c22a2c4da318844210925fbb2a0
Adam Florczak <adam.florczak@me.com>
2014-06-27 14:14:17 +0200

    Basic switch-if translation for crack time

--- js/zen.js
+++ js/zen.js
@@ -37,8 +37,10 @@ $(function() {
   output.on('input', function() {
     var password = output.val();
     var strength = zxcvbn(password);
+    var human_crack_time = translateCrackTime(strength.crack_time_display);
+
     $('body').attr('class', 'strength-' + strength.score);
-    strength_output.text(strength.crack_time_display);
+    strength_output.text(human_crack_time);
   });
   output.on('click', function() {
     $(this).select();
@@ -57,8 +59,6 @@ $(function() {
     output.val(password).trigger('input');
   }
 });
-
-
 
 function separateString(string, separator) {
   string.split('');
@@ -82,4 +82,70 @@ function separateString(string, separator) {
 
 function randomNumber(max, min) {
   return (Math.floor(Math.random() * 100) % (max - min)) + min;
+}
+
+// we have pretty difficult language here in Poland (:
+function translateCrackTime(sentence) {
+  var number = sentence.match(/\d+/);
+  var word = sentence.replace(number, '').trim();
+
+  var translation;
+  switch (word) {
+    case 'instant':
+      translation = 'migiem';
+      break;
+    case 'minute':
+    case 'minutes':
+      if (number == 1)
+        translation = 'minuta';
+      else if (number == 2)
+        translation = 'minuty';
+      else if ($.inArray(number, [3,4]) >= 0)
+        translation = 'minuty';
+      else
+        translation = 'minut';
+      break;
+    case 'hour':
+    case 'hours':
+      if (number == 1)
+        translation = 'godzina';
+      else if ($.inArray(number, [2,3,4]) >= 0)
+        translation = 'godziny';
+      else
+        translation = 'godzin';
+      break;
+    case 'day':
+    case 'days':
+      if (number == 1)
+        translation = 'dzień';
+      else
+        translation = 'dni';
+      break;
+    case 'month':
+    case 'months':
+      if (number == 1)
+        translation = 'miesiąc';
+      else if ($.inArray(number, [2,3,4]) >= 0)
+        translation = 'miesiące';
+      else
+        translation = 'miesięcy';
+      break;
+    case 'year':
+    case 'years':
+      if (number == 1)
+        translation = 'rok';
+      else if ($.inArray(number, [2,3,4]) >= 0)
+        translation = 'lata';
+      else
+        translation = 'lat';
+      break;
+    case 'centuries':
+      translation = 'wieki';
+      break;
+    default:
+      translation = word;
+      break;
+  }
+
+  return sentence.replace(word, translation);
 }
